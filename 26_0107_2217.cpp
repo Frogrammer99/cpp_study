e923af69d16ef83c9fc43920447f652e9953efef
Moon ChangWuk <moonchang0193@gmail.com>
2026-01-07 22:58:45 +0900

    2026_0107_random

---
+++ 26_0107_2217.cpp
@@ -0,0 +1,44 @@
+#include <iostream>	
+#include <random>
+using namespace std;
+
+class Random {
+private:
+
+public:
+	Random();
+	mt19937& getEngine();
+	int next();
+	int nextInRange(int n1, int n2);
+};
+Random::Random() { }
+
+mt19937& Random::getEngine() {
+	static random_device rd;
+	static mt19937 gen(rd());
+	return gen;
+}
+
+int Random::next() {
+	uniform_int_distribution<int> dis(0, 32767);
+	return dis(getEngine());
+}
+
+int Random::nextInRange(int n1, int n2) {
+	uniform_int_distribution<int>dis(n1, n2);
+	return dis(getEngine());
+}
+
+int main() {
+	Random r;
+	cout << "--0에서 32767까지의 정수 10개--" << endl;
+	for (int i = 0; i < 10; i++) {
+		int n = r.next();
+		cout << "n: " << n << "  ";
+	}
+	cout << endl << endl << "--2에서 4까지의 정수 10개--" << endl;
+	for (int i = 0; i < 10; i++) {
+		int n = r.nextInRange(2, 4);
+		cout << "n: " << n << "  ";
+	}
+}
