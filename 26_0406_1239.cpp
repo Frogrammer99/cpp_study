cdc7d4f2ade9147ced606d6d70648a06c4da8116
Moon ChangWuk <moonchang0193@gmail.com>
2026-04-06 23:30:05 +0900

    2026_0406_prtc

---
+++ 26_0406_1239.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+void hanoiTower(int num, char from, char by, char to) {
+	if (num == 1)
+		std::cout << "원반 " << num << "을 " << from << "에서 " << to << "로 이동" << std::endl;
+	else {
+
+	}
+}
