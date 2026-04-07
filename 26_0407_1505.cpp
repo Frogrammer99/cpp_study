082890fd55fb1d40f65d4229de323fb8809f195c
Moon ChangWuk <moonchang0193@gmail.com>
2026-04-07 21:04:55 +0900

    2026_0407_prtc

---
+++ 26_0407_1505.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <string>
+//
+//void hanoiTower(int num, char from, char by, char to) {
+//	if (num == 1) {
+//		std::cout << "원반 " << num << "을 " << from << "에서 " << to << "로 이동" << std::endl;
+//	}
+//	else {
+//		hanoiTower(num - 1, from, to, by);
+//		std::cout << "원반 " << num << "을 " << from << "에서 " << to << "로 이동" << std::endl;
+//		hanoiTower(num - 1, by, from, to);
+//	}
+//}
+//
+//int main() {
+//	hanoiTower(3, 'A', 'B', 'C');
+//}
+////////////////////////////////////////////////////////////////////////////
